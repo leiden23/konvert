784672f8bf80e05f63d0f0abd18e3c6f3aa3b803
Irina <kievskayaira23@gmail.com>
2026-08-19 14:55:46 +0300

    feat: added Button in Storybook

---
+++ src/shared/ui/button/ui/button.stories.ts
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Button } from "./button";
+
+const meta = {
+    title: 'Shared/Button',
+    component: Button,
+    tags: ['autodocs'],
+    parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Button>
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        variant: 'primary',
+        children: 'кнопка'
+    }
+}
+export const Secondary: Story = {
+    args: {
+        variant: 'secondary',
+        children: 'кнопка'
+    }
+}
+export const Tertiary: Story = {
+    args: {
+        variant: 'tertiary',
+        children: 'кнопка'
+    }
+}
